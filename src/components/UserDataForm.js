3efa82de3d6e19bcbe56529a40633c35778fb995
petagys <dack2702@rambler.ru>
2020-07-21 21:43:32 +0300

    finished first variant

--- src/components/UserDataForm.js
+++ src/components/UserDataForm.js
@@ -7,16 +7,16 @@ import Button from "@material-ui/core/Button/Button";
 import {inject, observer} from "mobx-react";
 
 const styles = {
-    block:{
+    block: {
         padding: '12px'
     },
-    block50:{
+    block50: {
         width: '50%'
     },
-    btn:{
+    btn: {
         backgroundColor: '#12D6E3',
         color: '#ffffff',
-        '&:hover':{
+        '&:hover': {
             backgroundColor: '#11a9b4'
         }
     }
@@ -27,7 +27,7 @@ const styles = {
 @observer
 class UserDataForm extends Component {
 
-    constructor(props){
+    constructor(props) {
         super(props);
         this.handleChangeSurname = this.handleChangeSurname.bind(this);
         this.handleChangeName = this.handleChangeName.bind(this);
@@ -47,38 +47,44 @@ class UserDataForm extends Component {
         this.props.user.setEmail(e.target.value)
     };
 
-    startTest(){
+    startTest() {
         this.props.user.checkUserData();
     }
 
     render() {
-        const {classes} = this.props;
+        const {classes, user} = this.props;
 
         return <Grid container justify={"center"}>
-                <Grid item xs={8}>
-                    <Paper style={{padding: '12px'}}>
-                        <div style={{textAlign: "center", marginBottom: '12px'}}>
-                            Для прохождения теста введите свои данные
+            <Grid item xs={8}>
+                <Paper style={{padding: '12px'}}>
+                    <div style={{textAlign: "center", marginBottom: '12px'}}>
+                        Для прохождения теста введите свои данные
+                    </div>
+                    <div style={{display: 'flex'}}>
+                        <div className={classes.block + ' ' + classes.block50}>
+                            <TextField helperText={!user.surnameFlag && 'Поле Фамилия не должно быть пустым'}
+                                       error={!user.surnameFlag} onChange={this.handleChangeSurname} fullWidth
+                                       variant={'outlined'} label={'Фамилия'} placeholder={'Иванов'}/>
                         </div>
-                        <div style={{display: 'flex'}}>
-                            <div className={classes.block + ' ' + classes.block50}>
-                                <TextField onChange={this.handleChangeSurname} fullWidth variant={'outlined'} label={'Фамилия'} placeholder={'Иванов'}/>
-                            </div>
-                            <div className={classes.block + ' ' + classes.block50}>
-                                <TextField onChange={this.handleChangeName} fullWidth variant={'outlined'} label={'Имя'} placeholder={'Иван'}/>
-                            </div>
+                        <div className={classes.block + ' ' + classes.block50}>
+                            <TextField helperText={!user.nameFlag && 'Поле Имя не должно быть пустым'}
+                                       error={!user.nameFlag} onChange={this.handleChangeName} fullWidth
+                                       variant={'outlined'} label={'Имя'} placeholder={'Иван'}/>
                         </div>
-                        <div className={classes.block}>
-                            <TextField onChange={this.handleChangeEmail} fullWidth variant={'outlined'} label={'E-mail'} placeholder={'ivanov@mail.ru'}/>
-                        </div>
-                        <div className={classes.block} style={{textAlign: 'center'}}>
-                            <Button onClick={this.startTest} className={classes.btn} variant="contained" >
-                                Начать тестирование
-                            </Button>
-                        </div>
-                    </Paper>
-                </Grid>
-            </Grid>;
+                    </div>
+                    <div className={classes.block}>
+                        <TextField helperText={!user.eMailFlag && 'Некорректно введен E-mail'} error={!user.eMailFlag}
+                                   onChange={this.handleChangeEmail} fullWidth variant={'outlined'} label={'E-mail'}
+                                   placeholder={'ivanov@mail.ru'}/>
+                    </div>
+                    <div className={classes.block} style={{textAlign: 'center'}}>
+                        <Button onClick={this.startTest} className={classes.btn} variant="contained">
+                            Начать тестирование
+                        </Button>
+                    </div>
+                </Paper>
+            </Grid>
+        </Grid>;
     }
 }
 
--- src/stores/UserInfoStore.js
+++ src/stores/UserInfoStore.js
@@ -1,49 +1,57 @@
-import { decorate, observable, action } from "mobx"
+import {decorate, observable, action} from "mobx"
 
 class UserInfoStore {
-    constructor(){
+    constructor() {
         this.surname = '';
+        this.surnameFlag = true;
         this.name = '';
+        this.nameFlag = true;
         this.eMail = '';
-        this.content = 'test';
+        this.eMailFlag = true;
+        this.content = 'reg';
     }
 
     @action('set user surname')
-    setSurname(value=''){
+    setSurname(value = '') {
         this.surname = value;
     }
 
     @action('set user name')
-    setName(value=''){
+    setName(value = '') {
         this.name = value;
     }
 
     @action('set user e-mail')
-    setEmail(value=''){
+    setEmail(value = '') {
         this.eMail = value;
     }
 
     @action('check all user data')
-    checkUserData(){
-        console.log('Фамилия: '+this.surname);
-        console.log('Имя: '+this.name);
-        console.log('Электронная почта: '+this.eMail);
-        if(this.surname && this.name && this.eMail){
+    checkUserData() {
+        this.surnameFlag = !!this.surname;
+        this.nameFlag = !!this.name;
+        let re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/i;
+        this.eMailFlag = re.test(this.eMail);
+        if (this.surnameFlag && this.nameFlag && this.eMailFlag) {
             this.setContent('test');
         }
     }
 
     @action('set page')
-    setContent(val='reg'){
+    setContent(val = 'reg') {
         this.content = val;
     }
 
 }
+
 decorate(UserInfoStore, {
     surname: observable,
     name: observable,
     eMail: observable,
     dataChecked: observable,
     content: observable,
+    nameFlag: observable,
+    surnameFlag: observable,
+    eMailFlag: observable,
 });
 export default new UserInfoStore();
